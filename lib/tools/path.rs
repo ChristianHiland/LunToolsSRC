70095b55a71379dd6591038daf5b5c4650f6aeb4
Lee Lunbin <hilandchristian112@gmail.com>
2024-11-14 12:47:46 -0500

    Update path.rs

--- lib/tools/path.rs
+++ lib/tools/path.rs
@@ -61,7 +61,8 @@ mod tests {
     // Tests The 'add_path' func.
     #[test]
     fn add_path_test() {
-        let result = add_to_PWD("tests\\data\\test.json".to_string(), false);
+        let result = add_to_PWD("tests/data/test.json".to_string(), false);
+        let expected_result = format!("{}/{}", get_pwd(), "tests/data/test.json");
         assert_eq!(result, "C:\\Users\\hilan\\Documents\\Code\\Rust\\LunTools\\LunToolsSRC\\tests\\data\\test.json".to_string());
     }
 }
