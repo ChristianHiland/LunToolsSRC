0cee2f5270277e1f4c44ca8e87b96719f82f5b3b
Lee Lunbin <hilandchristian112@gmail.com>
2024-11-14 10:57:26 -0500

    modified:   .github/workflows/rust.yml 	deleted:    tests/path_testing.rs 	modified:   tools/path.rs

--- tools/path.rs
+++ tools/path.rs
@@ -36,3 +36,32 @@ pub fn add_to_PWD(path: String, debug: bool) -> String {
     // 4. Returns the Current Working DIR with the 'path' added to the end.
     return added_path;
 }
+
+
+// Tests (Cargo)
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn path_test() {
+        println!("Path Test");
+    }
+    // Tests The 'get_pwd' func.
+    #[test]
+    fn get_pwd_test() {
+        path::get_pwd();
+    }
+    // Tests The 'get_exe' func.
+    #[test]
+    fn get_exe_tet() {
+        path::get_exe();
+    }
+    // Tests The 'add_path' func.
+    #[test]
+    fn add_path_test() {
+        let result = path::add_path("tests/data/test.json".to_string(), false);
+        assert_eq!(result, "C:\\Users\\hilan\\Documents\\Code\\Rust\\LunTools\\LunToolsSRC\\tests\\data\\test.json".to_string());
+    }
+}
