1fcc28babb0d55aabf0312674d3086f3f5714efa
Lee Lunbin <hilandchristian112@gmail.com>
2024-12-02 07:39:43 -0500

    common_tags.rs 업데이트

--- lib/vars/common_tags.rs
+++ lib/vars/common_tags.rs
@@ -5,7 +5,7 @@ use std::fmt;                                       // Importing Fmt from std.
 // Struts
 
 // file tags
-pub const file_type_images: [&str; 15] = [".jpg", ".jpeg", ".jifif", ".pjpeg", ".pjp", ".png", ".gif", ".tif", ".tiff", ".bmp", ".webp", ".svg", "eps", ".ai", ".psd"];
+pub const file_type_images: [&str; 13] = [".jpg", ".jpeg", ".jifif", ".pjpeg", ".pjp", ".png", ".gif", ".tif", ".tiff", ".bmp", ".webp", ".svg", "eps"];
 pub const file_type_program: [&str; 10] = [".py", ".js", ".java", ".cpp", ".cs", ".rs", ".rpy", ".h", ".asm", ".s"];
 pub const file_type_videos: [&str; 9] = [".mp4", ".mov", ".avi", ".wmv", ".mkv", ".webm", ".flv", ".mts", ".m2ts"];
 pub const file_type_audios: [&str; 8] = [".mp3", ".wav", ".aac", ".ogg", ".flac", ".aiff", ".aif", ".wma"];
@@ -15,3 +15,4 @@ pub const file_type_database: [&str; 3] = [".db", ".sqlite", ".mdb"];
 pub const file_type_config: [&str; 3] = [".ini", ".xml", ".json"];
 pub const file_type_system: [&str; 3] = [".dll", ".sys", ".inf"];
 pub const file_type_fontfile: [&str; 2] = [".ttf", ".otf"];
+pub const file_type_abobe: [&str; 2] = [".psd", "ai"];
